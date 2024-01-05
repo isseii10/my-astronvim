f239782ffe60f27cb3f9c717f586c95c2a94af0e
isseii10 <t1l.g2.o11g@gmail.com>
2024-01-06 00:16:26 +0900

    add astrocommunity

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,15 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
 }
