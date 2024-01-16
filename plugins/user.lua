c2954f6f40ad5acea70b604c43a2db6f7e374c63
isseii10 <t1l.g2.o11g@gmail.com>
2024-01-17 02:08:13 +0900

    add bamboo colorscheme

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,9 +9,10 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "neanias/everforest-nvim", version = false, lazy = false, priority = 1000 },
-  { "rebelot/kanagawa.nvim", priority = 1000 },
-  { "rose-pine/neovim", name = "rose-pine", priority = 1000 },
+  { "ellisonleao/gruvbox.nvim", priority = 1000,     config = true,  opts = ... },
+  { "catppuccin/nvim",          name = "catppuccin", priority = 1000 },
+  { "neanias/everforest-nvim",  version = false,     lazy = false,   priority = 1000 },
+  { "rebelot/kanagawa.nvim",    priority = 1000 },
+  { "rose-pine/neovim",         name = "rose-pine",  priority = 1000 },
+  { "ribru17/bamboo.nvim",      lazy = false,        priority = 1000 },
 }
