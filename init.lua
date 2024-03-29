d509677adfe0b754d4f12a39b93984deefdef656
isseii10 <t1l.g2.o11g@gmail.com>
2024-03-30 01:35:32 +0900

    use kanagawa schema

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin-mocha",
+  colorscheme = "kanagawa-wave",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
